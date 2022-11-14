49710ed5272604100df2dbd58145ec69a4c0dadc
SuperGandhi <patrickborjabueno@hotmail.com>
2022-11-14 12:41:43 -0500

    Controller register complete

--- login/resources/views/auth/register.blade.php
+++ login/resources/views/auth/register.blade.php
@@ -7,12 +7,12 @@
     <title>Register</title>
 </head>
 <body>
-    <form action="">
+    <form action="/register" method="POST">
         @csrf
         <input type="text" name="username">
         <input type="email" name="email">
         <input type="password" name="password">
-        <input type="text" name="password_confirmation">
+        <input type="password" name="password_confirmation">
         <input type="submit" value="register">
     </form>
 </body>
